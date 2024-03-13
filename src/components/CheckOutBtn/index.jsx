0c11437ca5c154e20ec366675e9f46895ac3ae61
mathiasg12 <mathiasgausl@gmail.com>
2024-03-13 09:51:44 +0100

    created cart page

--- src/components/CheckOutBtn/index.jsx
+++ src/components/CheckOutBtn/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './checkOutBtn.module.css';
+import { Link } from 'react-router-dom';
 /**
  * the check out button component
  */
 export function CheckOutBtn() {
-  return <button className={styles.checkOutBtn}>Checkout</button>;
+  return (
+    <Link className={styles.checkOutBtn} to="/checkOutSuccess">
+      Checkout
+    </Link>
+  );
 }
